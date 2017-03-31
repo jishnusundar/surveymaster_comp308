76eb3499dd5899092c8cff45aab06cb88762377b
aaujla55 <amandeep99555@gmail.com>
2017-03-31 15:17:13 -0400

    Created home page after login

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -1,4 +1,10 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/userSurveyList',(req,res,next)=>{
+    res.render('surveys/userSurveyList',{
+        title:'Your surveys'
+    });
+});
+
 module.exports = router;
