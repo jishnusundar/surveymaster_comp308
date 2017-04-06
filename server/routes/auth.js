b1992456e3372663eb1473d96f61f66b493d2f27
= <jishnusundar423@gmail.com>
2017-04-05 23:10:05 -0400

    Style Integration for header hyperlinks

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -16,7 +16,7 @@ router.get('/login',(req,res,next)=>{
 
 router.post('/login', passport.authenticate('local', {
 successRedirect: '/survey/userSurveyList',
-failureRedirect: '/login',
+failureRedirect: '/auth/login',
 failureFlash: true
 }));
 
