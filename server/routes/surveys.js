23061efee2ae3ae661a3a35b712b9948a01679e9
= <jishnusundar423@gmail.com>
2017-04-05 23:17:43 -0400

    Security Functionality added for authentication

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -3,7 +3,15 @@ let router = express.Router();
 
 let surveysController = require('../controllers/surveys');
 
-router.get('/userSurveyList',(req,res,next)=>{
+function requireAuth(req,res,next) {
+  //check if the user is logged in, else prompt to log in
+  if(!req.isAuthenticated()) {
+    return res.redirect('/auth/login');
+  }
+    next();
+}
+
+router.get('/userSurveyList',requireAuth,(req,res,next)=>{
    surveysController.displayUserSurveyPage(req,res,next);
 });
 
