801ff01f1d9dd9fe149044dbbaebe81c95b0c1b4
= <jishnusundar423@gmail.com>
2017-04-13 15:49:25 -0400

    Implemented True or False Survey

--- server/routes/respond.js
+++ server/routes/respond.js
@@ -3,14 +3,26 @@ let router = express.Router();
 
 let respondController = require('../controllers/respond');
 
-router.get('/:id',(req,res,next) => {
-respondController.displayResponsePage(req,res,next,req.params.id);
+router.get('/respondMCQ/:id',(req,res,next) => {
+respondController.displayMCQResponsePage(req,res,next,req.params.id);
 });
 
-router.post('/:id',(req,res,next) => {
-respondController.processResponse(req,res,next,req.params.id);
+router.post('/respondMCQ/:id',(req,res,next) => {
+respondController.processMCQResponse(req,res,next,req.params.id);
 });
 
+router.get('/identifyResponseType/:id',(req,res,next)=>{
 
+respondController.identifyResponseSurveyType(req,res,next);
+
+});
+
+router.get('/respondTF/:id', (req,res,next)=> {
+    respondController.displayTfResponsePage(req,res,next);
+});
+
+router.post('/respondTF/:id', (req,res,next)=> {
+    respondController.processTfResponse(req,res,next);
+});
 
 module.exports = router;
